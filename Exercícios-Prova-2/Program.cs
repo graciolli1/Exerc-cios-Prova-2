8e9706f71c53bb41455486e950b7166173d9fef9
Leo <Leo@DESKTOP-LU2I4O1>
2022-07-06 19:00:32 -0300

    Adicionar arquivos de projeto.

---
+++ Exercícios-Prova-2/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercícios_Prova_2
+{
+    internal class Program
+    {
+        
+        
+            /*Fazer um programa para que mostre uma lista de cidades para o usuário e que ele informe o código da cidade escolhida. Em seguida pedir uma data para o usuário. Ao final deve ser apresentada em tela a data completa em formato extenso longo, juntamente com a cidade escolhida. 
+        Exemplo: Bragança Paulista, 29 de junho de 2022.
+        A relação das cidades é a seguinte:
+        1 – Atibaia
+        2 – Bragança Paulista
+        3 – Mairiporã
+        4 – Nazaré
+        5 – Terra Preta
+        6 – Extrema
+        7 – Vargem
+        Requisitos: Criar uma função para converter a data em extenso.*/
+            public static void Main()
+            {
+                string DataAtual;
+                int cod;
+                Console.WriteLine("Digite o código da cidade: \n1 – Atibaia \n2 – Bragança Paulista \n3 – Mairiporã \n4 – Nazaré \n5 – Terra Preta \n6 – Extrema \n7 – Vargem");
+            cod = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Digite a data: [Formato: DD/MM/AAAA]");
+                DataAtual = Console.ReadLine();
+                Console.WriteLine(mesExtenso(Convert.ToDateTime(DataAtual)));
+                if (cod == 1)
+                {
+                    Console.Write("Atibaia");
+                }
+                else if (cod == 2)
+                {
+                    Console.Write("Bragança Paulista");
+                }
+                else if (cod == 3)
+                {
+                    Console.Write("Mairiporã");
+                }
+                else if (cod == 4)
+                {
+                    Console.Write("Nazaré");
+                }
+                else if (cod == 5)
+                {
+                    Console.Write("Terra Preta");
+                }
+                else if (cod == 6)
+                {
+                    Console.Write("Extrema");
+                }
+                else if (cod == 7)
+                {
+                    Console.Write("Vargem");
+                }
+                Console.ReadKey();
+            }
+            static public string mesExtenso(DateTime DataAtual)
+            {
+                int dia, mes, ano;
+                string mesExtenso = null;
+                mes = DataAtual.Month;
+                dia = DataAtual.Day;
+                ano = DataAtual.Year;
+                switch (mes)
+                {
+                    case 1:
+                        mesExtenso = "Janeiro";
+                        break;
+                    case 2:
+                        mesExtenso = "Fevereiro";
+                        break;
+                    case 3:
+                        mesExtenso = "Março";
+                        break;
+                    case 4:
+                        mesExtenso = "Abril";
+                        break;
+                    case 5:
+                        mesExtenso = "Maio";
+                        break;
+                    case 6:
+                        mesExtenso = "Junho";
+                        break;
+                    case 7:
+                        mesExtenso = "Julho";
+                        break;
+                    case 8:
+                        mesExtenso = "Agosto";
+                        break;
+                    case 9:
+                        mesExtenso = "Setembro";
+                        break;
+                    case 10:
+                        mesExtenso = "Outubro";
+                        break;
+                    case 11:
+                        mesExtenso = "Novembro";
+                        break;
+                    case 12:
+                        mesExtenso = "Dezembro";
+                        break;
+                }
+                return dia + " de " + mesExtenso + " de " + ano;
+
+            }
+
+        
+    }
+}
